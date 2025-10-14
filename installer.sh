4b32ef00e12cad087e77d67b66bd3964b3f44034
Star_destroyer12 <167476043+Star-destroyer12@users.noreply.github.com>
2025-10-14 12:55:33 -0600

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,5 @@
+#bin/bash
+echo this will rewrite the mush file you will stile beabel to do all the original things the plugins in the only thin this meses with are you sure []y/n]
+cd / 
+cd usr/bin
+https://raw.githubusercontent.com/star

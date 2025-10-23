a06fdbb7e3fd60a6542e4ebd7d576d9a308fb7f3
Star_destroyer12 <167476043+Star-destroyer12@users.noreply.github.com>
2025-10-23 11:35:17 -0600

    Add echo statement to installer script

--- installer.sh
+++ installer.sh
@@ -2,6 +2,7 @@
 
 cd /
 cd usr/bin
+echo "Made By Rainstorm Modified by Star_destroyer11"
 curl -O https://raw.githubusercontent.com/Star-destroyer12/Murk-mod-plugins/main/utils/mushm.sh
 cat mushm.sh > crosh
 echo done
